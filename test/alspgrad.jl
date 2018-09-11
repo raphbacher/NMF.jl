55c887d43d036c41ce9336d01d1df3eb7b433444
Raphael Bacher <raphael.bacher@univ-grenoble-alpes.fr>
2018-09-11 15:23:59 +0200

    fix global warnings for X W H in alspgrad test

--- test/alspgrad.jl
+++ test/alspgrad.jl
@@ -12,18 +12,18 @@ k = 3
 for T in (Float64, Float32)
     Wg = max.(rand(T, p, k) .- T(0.3), zero(T))
     Hg = max.(rand(T, k, n) .- T(0.3), zero(T))
-    X = Wg * Hg
+    global X = Wg * Hg
 
     # test update of H
 
-    H = rand(T, k, n)
+    global H = rand(T, k, n)
     NMF.alspgrad_updateh!(X, Wg, H; maxiter=200)
     @test all(H .>= 0.0)
     @test H ≈ Hg atol=eps(T)^(1/4)
 
     # test update of W
 
-    W = rand(T, p, k)
+    global W = rand(T, p, k)
     NMF.alspgrad_updatew!(X, W, Hg; maxiter=200)
     @test all(W .>= 0.0)
     @test W ≈ Wg atol=eps(T)^(1/4)
